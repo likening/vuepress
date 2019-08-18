579b243bf3f302061414d649e5e87a6c8c9bf516
likening <likening24@sina.com>
2019-08-18 14:37:54 +0800

    test

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    title: '黎科宁-码出世界',
+    description: 'PHP,JAVA,PYTHON',
+    head: [
+        ['link', { rel: 'icon', href: '/img/logo.ico' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ],
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '面试突击',
+              items: [
+                { text: '消息队列', link: '/mq/' },
+                { text: '分布式', link: '/ios/' },
+                { text: 'Web', link: '/web/' }
+              ]
+            },
+            { text: '关于', link: '/about/' },
+            { text: 'Github', link: 'https://www.github.com/codeteenager' },
+        ],
+        sidebar: {
+            '/mq/': [
+                        {
+                            title: '设计原则',
+                            collapsable: false,
+                            children: [
+                                ['/mq/001.md','MQ选择']
+                            ]
+                        }
+                ]
+            },
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated',
+    },
+}
